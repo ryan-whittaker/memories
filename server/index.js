0c9c56d96eea81f318d02ffa707a5d7cde1b97bf
Ryan Whittaker <rwhittaker>
2021-09-30 10:25:35 -0700

    Changed favicon

--- server/index.js
+++ server/index.js
@@ -11,11 +11,8 @@ dotenv.config()
 
 app.use("/posts", postRoutes)
 
-// Deprecated, so swapped bodyParser for express ?
-// https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(express.json({ limit: "30mb", extended: true }))
+app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
 const PORT = process.env.PORT || 5000
@@ -26,18 +23,6 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => error.message)
-
-// Deprecated
-// mongoose
-//   .connect(CONNECTION_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() =>
-//     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-//   )
-//   .catch((error) => error.message)
-// mongoose.set("useFindAndModify", false)
 
 // To kill the serverss
 // fuser -k -n tcp 5000
