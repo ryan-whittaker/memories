a4651c173284899849ce54d977059d818cbc5e10
Ryan Whittaker <rwhittaker@berkeley.edu>
2021-09-29 23:05:53 -0700

    Added dotenv

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
+import dotenv from "dotenv"
 
 import postRoutes from "./routes/posts.js"
 
 const app = express()
+dotenv.config()
 
 app.use("/posts", postRoutes)
 
@@ -16,12 +18,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
-const CONNECTION_URL =
-  "mongodb+srv://node-app:f9F5Y4FwoI1p0CUm@sandbox.orwek.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
 
 mongoose
-  .connect(CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL)
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
@@ -39,3 +39,6 @@ mongoose
 //   .catch((error) => error.message)
 
 // mongoose.set("useFindAndModify", false)
+
+// To kill the servers
+// fuser -k -n tcp 5000
