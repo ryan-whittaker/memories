792d454faaf045d5665db08e9fbcf1f8c3431c16
Ryan Whittaker <rwhittaker>
2021-09-30 22:47:23 -0700

    Added redux

--- server/index.js
+++ server/index.js
@@ -9,20 +9,18 @@ import postRoutes from "./routes/posts.js"
 const app = express()
 dotenv.config()
 
-app.use("/posts", postRoutes)
-
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+
+app.use("/posts", postRoutes)
 
 const PORT = process.env.PORT || 5000
 
 mongoose
   .connect(process.env.CONNECTION_URL)
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-  )
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => error.message)
 
-// To kill the serverss
+// To kill the servers
 // fuser -k -n tcp 5000
